f21a02860e10076526b3991b9dd3f17ea7c40e7d
Rodrigo <rodrigorahman@gmail.com>
2021-05-13 21:04:36 -0300

    alterando portas

--- bin/server.dart
+++ bin/server.dart
@@ -12,7 +12,7 @@ void main(List<String> args) async {
   var result = parser.parse(args);
 
   // For Google Cloud Run, we respect the PORT environment variable
-  var portStr = result['port'] ?? Platform.environment['PORT'] ?? '8080';
+  var portStr = result['port'] ?? Platform.environment['PORT'] ?? '443';
   var port = int.tryParse(portStr);
 
   if (port == null) {
