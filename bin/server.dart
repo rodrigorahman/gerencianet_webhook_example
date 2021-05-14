287eedb43778691540153406672aaf59a59661d7
Rodrigo <rodrigorahman@gmail.com>
2021-05-13 23:22:17 -0300

    method

--- bin/server.dart
+++ bin/server.dart
@@ -34,7 +34,7 @@ void main(List<String> args) async {
     print(request.url.hasAuthority);
     print(await request.readAsString());
     
-    if(request.requestedUri.hasAuthority && request.url.hasAuthority) {
+    if(request.url.hasAuthority) {
       return shelf.Response(401);
     }else {
       return shelf.Response(200);
